48170e7ccd70a7ac8f2ec673336d291388e26dd6
JY <kims2606@gmail.com>
2022-11-01 22:49:25 +0900

    타임리프 스프링 통합과 폼 - 라디오 버튼

--- part2/form/src/main/java/hello/itemservice/domain/item/ItemType.java
+++ part2/form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -15,4 +15,8 @@ public enum ItemType {
     ItemType(String description) {
         this.description = description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
