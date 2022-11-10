ef19d7b437ba16271d20b278363a80eb4ef68bc1
JY <kims2606@gmail.com>
2022-11-10 22:20:06 +0900

    검증2 - Bean Validation 오브젝트 오류

--- part2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ part2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//ObjectError는 검증하기에 제약사항이 많을 수 있으므로 자바 코드를 사용하자
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량의 합은 10,000원 이상이어야 합니다.")
 public class Item {
 
     private Long id;
