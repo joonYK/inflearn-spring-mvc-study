c49668ac262782df2af3a0a2e4cbde2f75bc609f
JY <kims2606@gmail.com>
2022-11-16 11:53:16 +0900

    로그인 처리2 (필터, 인터셉터) - 로그인 체크 필터 적용

--- part2/login/src/main/java/hello/login/WebConfig.java
+++ part2/login/src/main/java/hello/login/WebConfig.java
@@ -1,5 +1,6 @@
 package hello.login;
 
+import hello.login.web.LoginCheckFilter;
 import hello.login.web.filter.LogFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,15 @@ public class WebConfig {
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
         return filterRegistrationBean;
