1c2d1db318c199121ee79aa9a7c2741b7fe24e99
JY <kims2606@gmail.com>
2022-11-17 08:28:35 +0900

    예외 처리와 오류 페이지 - 인터셉터 (AWS의 오류 페이지 재호출은 동작 X)

--- part2/exception/src/main/java/hello/exception/WebConfig.java
+++ part2/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +14,17 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/css/**", "/*.ico", "/error",
+                                 //WAS의 오류 페이지 재호출시에는 인터셉터 도작 안하도록 적용
+                                 "/error-page/**");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
