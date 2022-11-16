373638599bd579e7a5e88103e9ba597bc4513334
JY <kims2606@gmail.com>
2022-11-16 23:48:20 +0900

    예외 처리와 오류 페이지 - http 상태 코드 및 예외에 따른 오류 페이지 제공

---
+++ part2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,23 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        //404일 때 에러 페이지
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        //500일 때 에러 페이지
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        //RuntimeException 또는 그 자식 타입일 때 에러 페이지
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
