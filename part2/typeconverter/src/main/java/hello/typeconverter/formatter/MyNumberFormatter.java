c480b0625d2582ff4eb04afb8b4688092946420c
JY <kims2606@gmail.com>
2022-11-18 00:03:50 +0900

    스프링 타입 컨버터 - 포맷터

---
+++ part2/typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,31 @@
+package hello.typeconverter.formatter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+/**
+ * Formatter 는 컨버터의 특별한 버전
+ * Converter : 범용(객체 -> 객체)
+ * Formatter : 문자에 특화(객체 -> 문자, 문자 -> 객체) + 현지화(Locale)
+ */
+@Slf4j
+public class MyNumberFormatter implements Formatter<Number> {
+
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}", text, locale);
+        //"1,000" -> 1000
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text);
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        return NumberFormat.getInstance(locale).format(object);
+    }
+}
