1032300c29dc633b726b798b68c65c62d513e9b9
JY <kims2606@gmail.com>
2022-11-17 21:40:40 +0900

    스프링 타입 컨버터 - Converter

---
+++ part2/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,12 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+}
