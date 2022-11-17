a08d52079988c2b3f875501cc937a3375f9289f6
JY <kims2606@gmail.com>
2022-11-17 22:31:17 +0900

    스프링 타입 컨버터 - 스프링에 Converter 적용

---
+++ part2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 스프링 내부에서 사용하는 ConversionService 에 컨버터를 추가
+     * Integer <-> String 같은 기본적인 컨버터들은 스프링에서 다 제공해주지만, 직접 등록한 컨버터가 우선적으로 사용됨.
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
